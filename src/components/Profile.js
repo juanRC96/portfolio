3f036b443f1afe768b11b07e3d0127b6b3ae4f85
juani <juanicuesta1996@gmail.com>
2022-08-12 17:30:42 -0300

    framer1

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import {motion} from "framer-motion";
 function Profile(){
 
     return(
-        <div className='perfil'> 
+        <motion.div className='perfil' initial={{ x:"-100%"}} animate={{x:"0%",transition:{duration: 2}}}> 
                 <motion.div className="foto" initial={{ opacity: 0,scale: 0,rotate:0 }} animate={{opacity: 1,transition:{duration: 1},rotate: 360,scale:1}} whileHover={{ scale: 1.1}} whileTap={{rotate:100}}>
                     <img src="images/profile.jpg" alt=""/>
                 </motion.div>
@@ -15,11 +15,11 @@ function Profile(){
                 
             </div>
             <div className="subtitulo">
-                <motion.h2 animate={{y:200,transition:{type:"spring",duration: 2}}} style={{color:"white",position:"relative",top:"-12rem"}}>
+                <motion.h2 animate={{x:"150%",transition:{type:"spring",duration: 4, delay:2}}}>
                     Full Stack Developer
                 </motion.h2>
             </div>
-        </div>
+        </motion.div>
     )
 }
 export default Profile;
